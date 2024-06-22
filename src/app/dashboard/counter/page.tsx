bbd0236ec3513437df973ff26a71935f9a64094d
jerlibgnzlz <jerlibgnzlz@gmail.com>
2024-06-22 20:08:42 -0300

    creando contador

--- src/app/dashboard/counter/page.tsx
+++ src/app/dashboard/counter/page.tsx
@@ -1,8 +1,31 @@
+"use client"
+import { useState } from "react";
 
 export default function NamePage() {
+
+
+    const [count, setCount] = useState(10)
+
+
     return (
-        <div>
-            <h1>Hello Page</h1>
+        <div className="flex flex-col items-center justify-center w-full h-full">
+            <span>Productos en el carrito</span>
+            <span className="mt-2 text-6xl">{count}</span>
+
+            <div className="flex mt-3">
+                <button
+                    onClick={() => { setCount(count + 1) }}
+                    className="flex items-center justify-center rounded-xl bg-gray-900 text-white hover:bg-gray-600 transition-all w-[100px] mr-2  ">
+                    +1
+                </button>
+
+                <button
+                    onClick={() => { setCount(count - 1) }}
+                    className="flex items-center justify-center rounded-xl bg-gray-900 text-white hover:bg-gray-600 transition-all w-[100px] mr-2 ">
+                    -1
+                </button>
+            </div>
+
         </div>
     );
 }
--- src/app/dashboard/main/page.tsx
+++ src/app/dashboard/main/page.tsx
@@ -1,7 +1,7 @@
 
 export default function MainPage() {
     return (
-        <div>
+        <div className="flex flex-col items-center justify-center w-full h-full">
             <h1>Hello MainPage</h1>
         </div>
     );
