a4bf5dbba47074a59e45ac3fa0a55984a203756f
jerlibgnzlz <jerlibgnzlz@gmail.com>
2024-06-22 20:41:00 -0300

    modularizando el componente contador

--- src/app/dashboard/counter/page.tsx
+++ src/app/dashboard/counter/page.tsx
@@ -1,31 +1,19 @@
-"use client"
-import { useState } from "react";
+import { CartCounter } from "@/app/shopping-cart";
+
+export const metadata = {
+    title: "Shopping Cart",
+    descripcion: "Contador"
+}
+
+
 
 export default function NamePage() {
-
-
-    const [count, setCount] = useState(10)
 
 
     return (
         <div className="flex flex-col items-center justify-center w-full h-full">
             <span>Productos en el carrito</span>
-            <span className="mt-2 text-6xl">{count}</span>
-
-            <div className="flex mt-3">
-                <button
-                    onClick={() => { setCount(count + 1) }}
-                    className="flex items-center justify-center rounded-xl bg-gray-900 text-white hover:bg-gray-600 transition-all w-[100px] mr-2  ">
-                    +1
-                </button>
-
-                <button
-                    onClick={() => { setCount(count - 1) }}
-                    className="flex items-center justify-center rounded-xl bg-gray-900 text-white hover:bg-gray-600 transition-all w-[100px] mr-2 ">
-                    -1
-                </button>
-            </div>
-
+            <CartCounter value={20} />
         </div>
     );
 }
