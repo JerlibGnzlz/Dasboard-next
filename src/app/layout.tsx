d51ae92e81025bc8f397d59ed246418ca1ea7b5e
jerlibgnzlz <jerlibgnzlz@gmail.com>
2024-06-22 17:02:03 -0300

    dasboard en camino

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -12,7 +12,7 @@ export const metadata: Metadata = {
 export default function RootLayout({ children, }: Readonly<{ children: React.ReactNode; }>) {
   return (
     <html lang="es">
-      <body classNameName={inter.classNameName}>{children}</body>
+      <body className={inter.className}>{children}</body>
     </html>
   );
 }
