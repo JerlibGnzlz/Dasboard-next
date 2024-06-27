f8c99ac9f14582255363edc2fd7373955c4a1e20
jerlibgnzlz <jerlibgnzlz@gmail.com>
2024-06-26 23:28:59 -0300

    buscando por un id

---
+++ src/app/dashboard/pokemon/[id]/page.tsx
@@ -0,0 +1,24 @@
+
+interface Props {
+    params: { id: string }
+}
+
+
+const getPokemons = async (id: string) => {
+    const pokemon = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}`,
+        { cache: "force-cache" }//TODO: cambiarlo mas adelante
+    )
+        .then(res => res.json())
+
+    return pokemon
+}
+
+export default async function PokemonPage({ params }: Props) {
+
+    const getPoke = await getPokemons(params.id)
+    return (
+        <div>
+            <h1>Pokemon {params.id}</h1>
+        </div>
+    );
+}
--- src/pokemons/components/PokemonCard.tsx
+++ src/pokemons/components/PokemonCard.tsx
@@ -28,7 +28,7 @@ export const PokemonCard = ({ pokemon }: Props) => {
                     <p className="pt-2 text-lg font-semibold text-gray-50 capitalize">{name}</p>
                     <div className="mt-5">
                         <Link
-                            className="border rounded-full py-2 px-4 text-xs font-semibold text-gray-100" href={`/dashboard/pokemon/${pokemon}`}                        >
+                            className="border rounded-full py-2 px-4 text-xs font-semibold text-gray-100" href={`/dashboard/pokemon/${id}`}                        >
                             Mas informacion
                         </Link>
                     </div>
